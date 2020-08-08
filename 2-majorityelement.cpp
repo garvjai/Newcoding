9668c4c6d020d257aa864c0d4763768148ee87ba
garv <garv.jain10@gmail.com>
2020-08-08 10:05:08 +0530

    messge

---
+++ 2-majorityelement.cpp
@@ -0,0 +1,14 @@
+/*Write a function which takes an array and prints the majority element (if it exists),
+ * otherwise prints “No Majority Element”. A majority element in an array A[] of size n
+ * is an element that appears more than n/2 times (and hence there is at most one such element).
+ */
+
+#include <iostream>
+
+using namespace std ;
+
+int main()
+{
+
+    return 0;
+}
