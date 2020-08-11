ed224a8d13083989ec4426a453cd4265e839ac74
garv <garv.jain10@gmail.com>
2020-08-11 17:06:07 +0530

    6 done

--- 6-findmissingnumber.cpp
+++ 6-findmissingnumber.cpp
@@ -2,8 +2,22 @@
 #include <iostream>
 using namespace std;
 
+int Find_miss(int array[], int length)
+{
+    int total = (length+1)*(length+1+1)/2;
+    for (int i = 0; i<length; i++)
+    {
+        total -=array[i];
+    }
+
+    return total;
+}
+
 int main()
 {
-
+    int arr [] = {1, 2, 4};
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int result = Find_miss(arr,length);
+    cout <<  result << endl;
     return 0;
 }
