662b624a9728d72fa615bc2d8d6c1735a0a8a403
garvjai <garv.jain10@gmail.com>
2020-08-12 20:36:44 +0530

    message

--- 6-findmissingnumber.cpp
+++ 6-findmissingnumber.cpp
@@ -14,11 +14,37 @@ int Find_miss(int array[], int length)
     return total;
 }
 
+/* 2nd method "the XOR technique" , in this xor all the elements of array and store it in a variable (a)
+ * and xor all the numbers from 1 to n+1 store it in a variable (b)
+ * then xor a and b to get the missing number
+ */
+int find_miss_xor(int arr[], int length)
+{
+    int a, b;
+    a = arr[0];
+    b = 1;
+    for(int i = 1; i<length; i++)
+    {
+        a ^= arr[i];
+    }
+    for(int i = 2; i<= length+1; i++)
+    {
+        b ^= i;
+    }
+    return a^b;
+
+}
+
 int main()
 {
     int arr [] = {1, 2, 4, 6, 9, 8, 3 ,5};
     int length = sizeof(arr)/sizeof(arr[0]);
     int result = Find_miss(arr,length);
-    cout <<  result << endl;
+    int result_xor = find_miss_xor(arr,length);
+
+    cout << "using sum formula technique " <<  result << endl;
+    cout << "using Xor technique " << result_xor << endl;
+
+
     return 0;
 }
