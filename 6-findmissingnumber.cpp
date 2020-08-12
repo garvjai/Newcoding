fb69943dd670437f44696bdd329471f1ae112ea9
garvjai <garv.jain10@gmail.com>
2020-08-12 10:42:59 +0530

    added comment to 6th question

--- 6-findmissingnumber.cpp
+++ 6-findmissingnumber.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+//1st method - using the formula n(n+1)/2 that is for the add of n numbers starting from 1;
 int Find_miss(int array[], int length)
 {
     int total = (length+1)*(length+1+1)/2;
@@ -15,7 +16,7 @@ int Find_miss(int array[], int length)
 
 int main()
 {
-    int arr [] = {1, 2, 4};
+    int arr [] = {1, 2, 4, 6, 9, 8, 3 ,5};
     int length = sizeof(arr)/sizeof(arr[0]);
     int result = Find_miss(arr,length);
     cout <<  result << endl;
