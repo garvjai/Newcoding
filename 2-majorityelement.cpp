cb58b55eab1ed58ccdda00e5f4cb3e9d00c0023e
garvjai <garv.jain10@gmail.com>
2020-08-25 12:05:53 +0530

    checking connectivit

--- 2-majorityelement.cpp
+++ 2-majorityelement.cpp
@@ -1,7 +1,6 @@
 ///*Write a function which takes an array and prints the majority element (if it exists),
 // * otherwise prints “No Majority Element”. A majority element in an array A[] of size n
 // * is an element that appears more than n/2 times (and hence there is at most one such element).
-// *
 // */
 //#include <iostream>
 //#include <map>
